dd0a1cfa4b6b118129263413a8296e18d8e48ef5
Brent Simmons <brent@ranchero.com>
2017-12-26 16:40:58 -0800

    Copy RSParser files from Evergreen to this standalone repository.

---
+++ OPML/RSOPMLDocument.m
@@ -0,0 +1,13 @@
+//
+//  RSOPMLDocument.m
+//  RSParser
+//
+//  Created by Brent Simmons on 2/28/16.
+//  Copyright © 2016 Ranchero Software, LLC. All rights reserved.
+//
+
+#import <RSParser/RSOPMLDocument.h>
+
+@implementation RSOPMLDocument
+
+@end
