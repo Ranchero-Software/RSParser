c2e8674f946be549990e89fb73136cd37cca9705
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2020-07-29 21:52:57 +0800

    RSParser is now the swift package

--- Package.swift
+++ Package.swift
@@ -9,11 +9,11 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "RSParserSwift",
-            targets: ["RSParserSwift"]),
-        .library(
             name: "RSParser",
             targets: ["RSParser"]),
+        .library(
+            name: "RSParser-ObjC",
+            targets: ["RSParser-ObjC"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,11 +23,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "RSParserSwift",
-            dependencies: ["RSParser"],
+            name: "RSParser",
+            dependencies: ["RSParser-ObjC"],
             path: "Sources/Swift"),
         .target(
-            name: "RSParser",
+            name: "RSParser-ObjC",
             dependencies: [],
             path: "Sources/ObjC",
             cSettings: [
